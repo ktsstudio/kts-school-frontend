70949deefff7169fdec69e583fa9c5f8884ec5a8
Виталий Черков <vitaly.cherkov@gmail.com>
2021-02-04 17:37:39 +0300

    lecture-2

---
+++ lecture-2/src/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header></header>
+    </div>
+  );
+}
+
+export default App;
---
+++ lecture-2/src/App/pages/DetailPage/DetailPage.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const DetailPage: React.FC = () => {
+    return <div></div>;
+};
+
+export default DetailPage;
